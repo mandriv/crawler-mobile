683287343b0e700ae9abd4b75c62492df090ba5c
mandriv <wojciech.cichoradzki@gmail.com>
2018-02-02 19:26:57 +0000

    Added transparentWhite colour

--- src/config/colours.js
+++ src/config/colours.js
@@ -5,3 +5,8 @@ export const transparent = 'transparent';
 // Palette
 export const peterRiver = '#3498db';
 export const belizeHole = '#2980b9';
+
+// Transparents
+export function whiteTransparent(opacity = 1.0) {
+  return `rgba(255, 255, 255, ${opacity})`;
+}
