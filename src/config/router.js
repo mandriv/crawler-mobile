ac0b223b755c809ee7bcde0cc6f02242d0903354
mandriv <wojciech.cichoradzki@gmail.com>
2018-02-04 13:48:44 +0000

    Add containers to router, create drawer nav

--- src/config/router.js
+++ src/config/router.js
@@ -1,8 +1,27 @@
-import { StackNavigator } from 'react-navigation';
+import React from 'react';
+import { StackNavigator, DrawerNavigator, NavigationActions } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Feather';
 // screens
 import SplashScreen from '../containers/Splash';
 import LoginScreen from '../containers/Login';
+import RegistrationScreen from '../containers/Registration';
+import DashboardScreen from '../containers/Dashboard';
 
+// resets stack
+export const resetAction = route => NavigationActions.reset({
+  index: 0,
+  actions: [NavigationActions.navigate({ routeName: route })],
+});
+
+const MainDrawer = DrawerNavigator({
+  Dashboard: {
+    screen: DashboardScreen,
+    navigationOptions: {
+      drawerLabel: 'Dashboard'.toUpperCase(),
+      drawerIcon: ({ tintColor }) => (<Icon name="home" color={tintColor} size={25}/>), // eslint-disable-line
+    },
+  },
+});
 
 export const RootNavigator = StackNavigator({
   Splash: {
@@ -18,7 +37,18 @@ export const RootNavigator = StackNavigator({
     navigationOptions: {
       header: false,
       title: 'Login',
-      gesturesEnabled: false,
+      gesturesEnabled: true,
     },
+  },
+  Registration: {
+    screen: RegistrationScreen,
+    navigationOptions: {
+      header: false,
+      title: 'Registration',
+      gesturesEnabled: true,
+    },
+  },
+  Main: {
+    screen: MainDrawer,
   },
 });
